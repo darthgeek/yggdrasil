1ee4bd065114423f7a5f7b8f8bf43202ddcfa81d
Jason Steele <jasonesteele@gmail.com>
2016-07-16 10:30:59 -0400

    Completed phaser tutorial

--- yggdrasil-javascript/src/main/resources/META-INF/resources/js/page/home.js
+++ yggdrasil-javascript/src/main/resources/META-INF/resources/js/page/home.js
@@ -4,7 +4,7 @@ require('phaser-shim');
 
 // https://github.com/photonstorm/phaser/issues/1974
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
   log.info('Phaser preload');
@@ -13,6 +13,13 @@ function preload() {
   game.load.image('star', 'images/star.png');
   game.load.spritesheet('dude', 'images/dude.png', 32, 48);
 }
+
+var player;
+var platforms;
+var cursors;
+var stars;
+var score = 0;
+var scoreText;
 
 function create() {
   log.info('Phaser create');
@@ -45,8 +52,82 @@ function create() {
   ledge = platforms.create(-150, 250, 'ground');
 
   ledge.body.immovable = true;
+
+// The player and its settings
+  player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+  //  We need to enable physics on the player
+  game.physics.arcade.enable(player);
+
+  //  Player physics properties. Give the little guy a slight bounce.
+  player.body.bounce.y = 0.2;
+  player.body.gravity.y = 300;
+  player.body.collideWorldBounds = true;
+
+  //  Our two animations, walking left and right.
+  player.animations.add('left', [0, 1, 2, 3], 10, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  cursors = game.input.keyboard.createCursorKeys();
+
+  stars = game.add.group();
+
+  stars.enableBody = true;
+
+  //  Here we'll create 12 of them evenly spaced apart
+  for (var i = 0; i < 12; i++)
+  {
+    //  Create a star inside of the 'stars' group
+    var star = stars.create(i * 70, 0, 'star');
+
+    //  Let gravity do its thing
+    star.body.gravity.y = 100;
+
+    //  This just gives each star a slightly random bounce value
+    star.body.bounce.y = 0.7 + Math.random() * 0.2;
+  }
+
+  scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+}
+
+function collectStar (player, star) {
+  // Removes the star from the screen
+  star.kill();
+
+  //  Add and update the score
+  score += 10;
+  scoreText.text = 'Score: ' + score;
 }
 
 function update() {
-  log.trace('Phaser update');
+  game.physics.arcade.collide(player, platforms);
+  game.physics.arcade.collide(stars, platforms);
+  game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+  //  Reset the players velocity (movement)
+  player.body.velocity.x = 0;
+
+  if (cursors.left.isDown) {
+    //  Move to the left
+    player.body.velocity.x = -150;
+
+    player.animations.play('left');
+  }
+  else if (cursors.right.isDown) {
+    //  Move to the right
+    player.body.velocity.x = 150;
+
+    player.animations.play('right');
+  }
+  else {
+    //  Stand still
+    player.animations.stop();
+
+    player.frame = 4;
+  }
+
+  //  Allow the player to jump if they are touching the ground.
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.body.velocity.y = -350;
+  }
 }
