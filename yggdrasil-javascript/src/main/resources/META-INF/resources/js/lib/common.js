774cbb7c823c94ec666b9a2cdab5a67caa2efa76
Jason Steele <jasonesteele@gmail.com>
2016-07-20 09:33:02 -0400

    Fixes #40.  Added gretty plugin and basic configuration.

--- yggdrasil-javascript/src/main/resources/META-INF/resources/js/lib/common.js
+++ yggdrasil-javascript/src/main/resources/META-INF/resources/js/lib/common.js
@@ -1,6 +1,6 @@
 /* global __webpack_public_path__ */
 
-// Standard stuff for every page
+//noinspection JSAnnotator
 __webpack_public_path__ = window._contextPath + "js/"; // eslint-disable-line camelcase
 
 var $ = window["jQuery"] = window.jquery = require("jquery");
