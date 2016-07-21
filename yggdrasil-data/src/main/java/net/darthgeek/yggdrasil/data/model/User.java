380ea67a35e6b63308bb4de0c60850aee822272f
Jason Steele <jasonesteele@gmail.com>
2016-07-21 12:42:44 -0400

    Fixing Codacy warning.

--- yggdrasil-data/src/main/java/net/darthgeek/yggdrasil/data/model/User.java
+++ yggdrasil-data/src/main/java/net/darthgeek/yggdrasil/data/model/User.java
@@ -3,7 +3,6 @@ package net.darthgeek.yggdrasil.data.model;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
