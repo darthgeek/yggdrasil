20987538af4a312dada5bed8551dccb9e03d0dc9
Jason Steele <jasonesteele@gmail.com>
2016-08-02 22:24:53 -0400

    Adding a call to webpack as browsersync starts up to make sure code is compiled.

--- yggdrasil-javascript/gulpfile.js/tasks/browsersync-dev.js
+++ yggdrasil-javascript/gulpfile.js/tasks/browsersync-dev.js
@@ -4,11 +4,11 @@ var webpack = require("webpack");
 var config = require("../config");
 var browserSync = require("browser-sync").create();
 
-gulp.task("browsersync:refresh", ["webpack:dev"], function() {
+gulp.task("browsersync:refresh", ["webpack:dev"], function () {
   browserSync.reload();
 });
 
-gulp.task("browsersync:dev", function (cb) {
+gulp.task("browsersync:dev", ["webpack:dev"], function (cb) {
   browserSync.init({
     proxy: "http://localhost:8080"
   });
