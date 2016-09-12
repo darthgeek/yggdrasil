2ffbe643d6147cd03308c9b0b2bf4a7dc4bdcf2e
Jason Steele <jasonesteele@gmail.com>
2016-09-11 21:54:13 -0400

    YGG-22 Reworked system menu into Bootstrap/AdminLTE pop-in pane and got rid of Phaser sprites/buttons.  Implemented logout menu item.

--- yggdrasil-javascript/gulpfile.js/tasks/build-dev.js
+++ yggdrasil-javascript/gulpfile.js/tasks/build-dev.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var sequence = require("gulp-sequence");
 
 gulp.task("build:dev", function (cb) {
-  sequence("webpack:dev", ["css", "font"], cb);
+  sequence("webpack:dev", ["css", "font", "hbs"], cb);
 });
 
 gulp.task("build_dev", ["build:dev"]);
